535e1d68fb47e9bc5f8c1fb028ba2611ce14177e
jenaruDev <jenaru.dev@gmail.com>
2023-02-09 12:08:13 +0100

    Fin seccion 6

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-grafica1',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   ]
 })
 export class Grafica1Component {
+
+ public labels1: string[] =[ 'Pan', 'Refrescos', 'Tacos' ]
+ public data1: ChartData<'doughnut'> = {
+  labels: this.labels1,
+  datasets: [
+    { data: [ 10, 15, 40 ],
+      borderColor: '#472836',
+      backgroundColor: ['#6857E6','#009FEE','#F02059'],
+     }
+  ]
+};
 
 }
